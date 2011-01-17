450b4b29f76f9eb44c3ddb3bed4db8f1af32d52f
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-01-17 13:24:50 -0500

    Refactoring part 1

--- product/nothinbutdotnetprep/collections/MovieLibrary.cs
+++ product/nothinbutdotnetprep/collections/MovieLibrary.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using nothinbutdotnetprep.infrastructure;
 
 namespace nothinbutdotnetprep.collections
 {
@@ -14,38 +15,25 @@ namespace nothinbutdotnetprep.collections
 
         public IEnumerable<Movie> all_movies()
         {
-            return movies;
+            return movies.one_at_a_time();
         }
 
         public void add(Movie movie)
         {
-            if (movies.Contains(movie))
-                return;
-            
-            foreach (Movie m in movies)
-            {
-
-                if (m.title == movie.title)
-                    return;
-            }
+            if (already_contains(movie)) return;
 
             movies.Add(movie);
         }
 
-        public IEnumerable<Movie> sort_all_movies_by_title_descending
+        bool already_contains(Movie movie)
         {
-           get
-           {
-               MovieComparerTitleDescending comp = new MovieComparerTitleDescending();
-               ((List<Movie>)movies).Sort(comp);
-               return movies;
-           }
+            return movies.Contains(movie);
         }
 
         public IEnumerable<Movie> all_movies_published_by_pixar()
         {
             IList<Movie> ret = new List<Movie>();
-            foreach (Movie m in movies)
+            foreach (var m in movies)
             {
                 if (m.production_studio == ProductionStudio.Pixar)
                     ret.Add(m);
@@ -56,7 +44,7 @@ namespace nothinbutdotnetprep.collections
         public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
         {
             IList<Movie> ret = new List<Movie>();
-            foreach (Movie m in movies)
+            foreach (var m in movies)
             {
                 if (m.production_studio == ProductionStudio.Pixar || m.production_studio == ProductionStudio.Disney)
                     ret.Add(m);
@@ -64,25 +52,10 @@ namespace nothinbutdotnetprep.collections
             return ret;
         }
 
-        public IEnumerable<Movie> sort_all_movies_by_title_ascending
-        {
-            get
-            {
-                MovieComparerTitleAscending comp = new MovieComparerTitleAscending();
-                ((List<Movie>)movies).Sort(comp);
-                return movies;
-            }
-        }
-
-        public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
-        {
-              throw new NotImplementedException(); 
-        }
-
         public IEnumerable<Movie> all_movies_not_published_by_pixar()
         {
             IList<Movie> ret = new List<Movie>();
-            foreach (Movie m in movies)
+            foreach (var m in movies)
             {
                 if (m.production_studio != ProductionStudio.Pixar)
                     ret.Add(m);
@@ -90,10 +63,35 @@ namespace nothinbutdotnetprep.collections
             return ret;
         }
 
+        public IEnumerable<Movie> sort_all_movies_by_title_descending
+        {
+            get
+            {
+                var comp = new MovieComparerTitleDescending();
+                ((List<Movie>) movies).Sort(comp);
+                return movies;
+            }
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_title_ascending
+        {
+            get
+            {
+                var comp = new MovieComparerTitleAscending();
+                ((List<Movie>) movies).Sort(comp);
+                return movies;
+            }
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<Movie> all_movies_published_after(int year)
         {
             IList<Movie> ret = new List<Movie>();
-            foreach (Movie m in movies)
+            foreach (var m in movies)
             {
                 if (m.date_published.Year > year)
                     ret.Add(m);
@@ -104,7 +102,7 @@ namespace nothinbutdotnetprep.collections
         public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
         {
             IList<Movie> ret = new List<Movie>();
-            foreach (Movie m in movies)
+            foreach (var m in movies)
             {
                 if (m.date_published.Year >= startingYear && m.date_published.Year <= endingYear)
                     ret.Add(m);
@@ -115,7 +113,7 @@ namespace nothinbutdotnetprep.collections
         public IEnumerable<Movie> all_kid_movies()
         {
             IList<Movie> ret = new List<Movie>();
-            foreach (Movie m in movies)
+            foreach (var m in movies)
             {
                 if (m.genre == Genre.kids)
                     ret.Add(m);
@@ -126,7 +124,7 @@ namespace nothinbutdotnetprep.collections
         public IEnumerable<Movie> all_action_movies()
         {
             IList<Movie> ret = new List<Movie>();
-            foreach (Movie m in movies)
+            foreach (var m in movies)
             {
                 if (m.genre == Genre.action)
                     ret.Add(m);
@@ -136,15 +134,15 @@ namespace nothinbutdotnetprep.collections
 
         public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
         {
-            MovieComparerDatePublishedDescending comp = new MovieComparerDatePublishedDescending();
-            ((List<Movie>)movies).Sort(comp);
+            var comp = new MovieComparerDatePublishedDescending();
+            ((List<Movie>) movies).Sort(comp);
             return movies;
         }
 
         public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
         {
-            MovieComparerDatePublishedAscending comp = new MovieComparerDatePublishedAscending();
-            ((List<Movie>)movies).Sort(comp);
+            var comp = new MovieComparerDatePublishedAscending();
+            ((List<Movie>) movies).Sort(comp);
             return movies;
         }
     }
