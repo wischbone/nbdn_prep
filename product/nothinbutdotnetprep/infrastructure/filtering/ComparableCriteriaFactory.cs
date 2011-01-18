8823dee590ef3a383f69a80c82e7377d23a90f2d
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-01-18 10:04:09 -0500

    Refactoring the AnonymousCriteria usage

--- product/nothinbutdotnetprep/infrastructure/filtering/ComparableCriteriaFactory.cs
+++ product/nothinbutdotnetprep/infrastructure/filtering/ComparableCriteriaFactory.cs
@@ -2,16 +2,41 @@ using System;
 
 namespace nothinbutdotnetprep.infrastructure.filtering
 {
-    public class ComparableCriteriaFactory<ItemToFilter, PropertyType> where PropertyType : IComparable<PropertyType>
+    public class ComparableCriteriaFactory<ItemToFilter, PropertyType>  : CriteriaFactory<ItemToFilter,PropertyType>where PropertyType : IComparable<PropertyType>
     {
+        PropertyAccessor<ItemToFilter, PropertyType> accessor;
+        CriteriaFactory<ItemToFilter, PropertyType> original_factory;
+
+        public ComparableCriteriaFactory(PropertyAccessor<ItemToFilter, PropertyType> accessor, CriteriaFactory<ItemToFilter, PropertyType> original_factory)
+        {
+            this.accessor = accessor;
+            this.original_factory = original_factory;
+        }
+
+        public Criteria<ItemToFilter> equal_to(PropertyType value)
+        {
+            return original_factory.equal_to(value);
+        }
+
+        public Criteria<ItemToFilter> equal_to_any(params PropertyType[] values)
+        {
+            return original_factory.equal_to_any(values);
+        }
+
+        public Criteria<ItemToFilter> not_equal_to(PropertyType value)
+        {
+            return original_factory.not_equal_to(value);
+        }
+
         public Criteria<ItemToFilter> greater_than(PropertyType value)
         {
-            throw new NotImplementedException();
+            return new AnononymousCriteria<ItemToFilter>(x => accessor(x).CompareTo(value) > 0);
         }
 
         public Criteria<ItemToFilter> between(PropertyType start, PropertyType end)
         {
-            throw new NotImplementedException();
+            return new AnononymousCriteria<ItemToFilter>(x => accessor(x).CompareTo(start) >= 0 &&
+                accessor(x).CompareTo(end) <= 0);
         }
     }
 }
--- product/nothinbutdotnetprep/infrastructure/filtering/CriteriaFactory.cs
+++ product/nothinbutdotnetprep/infrastructure/filtering/CriteriaFactory.cs
@@ -1,30 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace nothinbutdotnetprep.infrastructure.filtering
+﻿namespace nothinbutdotnetprep.infrastructure.filtering
 {
-    public class CriteriaFactory<ItemToFilter, PropertyType>
+    public interface CriteriaFactory<ItemToFilter, PropertyType>
     {
-        PropertyAccessor<ItemToFilter, PropertyType> property_accessor;
-
-        public CriteriaFactory(PropertyAccessor<ItemToFilter, PropertyType> property_accessor)
-        {
-            this.property_accessor = property_accessor;
-        }
-
-        public Criteria<ItemToFilter> equal_to(PropertyType value)
-        {
-            return equal_to_any(value);
-        }
-
-        public Criteria<ItemToFilter> equal_to_any(params PropertyType[] values)
-        {
-            return
-                new AnononymousCriteria<ItemToFilter>(x => new List<PropertyType>(values).Contains(property_accessor(x)));
-        }
-
-        public Criteria<ItemToFilter> not_equal_to(PropertyType value)
-        {
-            return new NotCriteria<ItemToFilter>(equal_to(value));
-        }
+        Criteria<ItemToFilter> equal_to(PropertyType value);
+        Criteria<ItemToFilter> equal_to_any(params PropertyType[] values);
+        Criteria<ItemToFilter> not_equal_to(PropertyType value);
     }
 }
